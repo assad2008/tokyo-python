cf772989f0eca5c7c1e82fb0acd693a3a66c968a
lekmalek <lekmalek@04fc1989-d439-828d-421d-ebc0a24ad0dc>
2010-06-30 10:22:30 +0000

    - added iterkeys(), itervalues() and iteritems() to NDB

--- tokyo-python/test/test_NDB.py
+++ tokyo-python/test/test_NDB.py
@@ -1,19 +1,7 @@
 import unittest
 import sys
 
-from tokyo.cabinet import NDB as _NDB, Error
-
-
-class NDB(_NDB):
-
-    def keys(self):
-        return (key for key in self)
-
-    def values(self):
-        return (self[key] for key in self)
-
-    def items(self):
-        return ((key, self[key]) for key in self)
+from tokyo.cabinet import NDB, Error
 
 
 class NDBTest(unittest.TestCase):
@@ -65,15 +53,15 @@ class NDBTestDict(NDBTest):
         self.db[b"a"] = b"1"
         self.db[b"b"] = b"2"
         self.assertEqual(len(self.db), 2)
-        d = dict(self.db.items())
+        d = dict(self.db.iteritems())
         self.assertEqual(d, {b"a": b"1", b"b": b"2"})
         self.db[b"c"] = b"3"
         self.db[b"a"] = b"4"
-        d = dict(self.db.items())
+        d = dict(self.db.iteritems())
         self.assertEqual(d, {b"a": b"4", b"b": b"2", b"c": b"3"})
         del self.db[b"b"]
         self.assertEqual(len(self.db), 2)
-        d = dict(self.db.items())
+        d = dict(self.db.iteritems())
         self.assertEqual(d, {b"a": b"4", b"c": b"3"})
 
     def test_clear(self):
@@ -82,21 +70,19 @@ class NDBTestDict(NDBTest):
         self.assertEqual(len(self.db), 2)
         self.db.clear()
         self.assertEqual(len(self.db), 0)
-        d = dict(self.db.items())
+        d = dict(self.db.iteritems())
         self.assertEqual(d, {})
 
 
 class NDBTestIter(NDBTest):
 
     def test_iter(self):
-        i = iter(self.db)
-        self.assertEqual(len(i), 0)
         self.db[b"a"] = b"1"
         self.db[b"b"] = b"2"
         self.db[b"c"] = b"3"
         i = iter(self.db)
-        self.assertEqual(len(i), len(self.db))
         self.assertTrue(b"a" in i)
+        i = iter(self.db)
         self.assertEqual([b"a", b"b", b"c"], sorted(i))
         i = iter(self.db)
         a = next(i)
@@ -105,12 +91,39 @@ class NDBTestIter(NDBTest):
         self.assertRaises(StopIteration, next, i)
         self.assertEqual([b"a", b"b", b"c"], sorted((a, b, c)))
         i = iter(self.db)
-        self.assertEqual(len(i), 3)
         a = next(i)
         del self.db[b"b"]
-        self.db[b"d"] = b"4"
-        self.assertEqual(len(i), 3)
         self.assertRaises(Error, next, i)
+        i = iter(self.db)
+        a = next(i)
+        self.db[b"d"] = b"4"
+        self.assertRaises(Error, next, i)
+        i = iter(self.db)
+        a = next(i)
+        del self.db[b"d"]
+        self.db[b"d"] = b"5"
+        self.assertRaises(Error, next, i)
+
+    def test_iterkeys(self):
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.db[b"c"] = b"3"
+        self.assertEqual([b"a", b"b", b"c"],
+                         sorted(list(self.db.iterkeys())))
+
+    def test_itervalues(self):
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.db[b"c"] = b"3"
+        self.assertEqual([b"1", b"2", b"3"],
+                         sorted(list(self.db.itervalues())))
+
+    def test_iteritems(self):
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.db[b"c"] = b"3"
+        self.assertEqual({b"a": b"1", b"b": b"2", b"c": b"3"},
+                         dict(self.db.iteritems()))
 
 
 class NDBTestPut(NDBTest):
