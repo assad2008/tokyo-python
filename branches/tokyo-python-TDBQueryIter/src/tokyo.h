9c429f9954db97531ed191349cab9a76817a0a84
lekmalek <lekmalek@04fc1989-d439-828d-421d-ebc0a24ad0dc>
2010-07-02 13:03:18 +0000

    TDBQueryIter branch

---
+++ branches/tokyo-python-TDBQueryIter/src/tokyo.h
@@ -0,0 +1,311 @@
+/*******************************************************************************
+*
+* Copyright (c) 2010, Malek Hadj-Ali
+* All rights reserved.
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*
+*******************************************************************************/
+
+
+#ifndef _TOKYO_PYTHON_H
+#define _TOKYO_PYTHON_H
+
+
+#define PY_SSIZE_T_CLEAN
+#include "Python.h"
+
+#include <tcutil.h>
+
+
+/*******************************************************************************
+* objects
+*******************************************************************************/
+
+/* Error */
+static PyObject *Error;
+
+
+/*******************************************************************************
+* utilities
+*******************************************************************************/
+
+/* error handling utils */
+PyObject *
+set_error(PyObject *type, const char *message)
+{
+    PyErr_SetString(type, message);
+    return NULL;
+}
+
+#define set_key_error(key) set_error(PyExc_KeyError, key)
+
+PyObject *
+set_stopiteration_error(void)
+{
+    PyErr_SetNone(PyExc_StopIteration);
+    return NULL;
+}
+
+
+#if PY_MAJOR_VERSION >= 3
+/* PyUnicode to char * */
+char *
+PyUnicode_AS_STRING(PyObject *unicode)
+{
+    PyObject *tmp;
+    char *result;
+
+    tmp = PyUnicode_EncodeUTF8(PyUnicode_AS_UNICODE(unicode),
+                               PyUnicode_GET_SIZE(unicode), NULL);
+    if (!tmp) {
+        return NULL;
+    }
+    result = PyBytes_AS_STRING(tmp);
+    Py_DECREF(tmp);
+    return result;
+}
+
+char *
+PyUnicode_AsString(PyObject *unicode)
+{
+    if (!PyUnicode_Check(unicode)) {
+        PyErr_SetString(PyExc_TypeError, "a string is required");
+        return NULL;
+    }
+    return PyUnicode_AS_STRING(unicode);
+}
+
+#define PyString_AsString PyUnicode_AsString
+#define PyString_FromString PyUnicode_FromString
+#define PyInt_FromLong PyLong_FromLong
+#endif
+
+
+/* convert a TCLIST to a fozenset */
+PyObject *
+tclist_to_frozenset(TCLIST *result)
+{
+    int len, i;
+    PyObject *pyresult, *pyvalue;
+
+    pyresult = PyFrozenSet_New(NULL);
+    if (!pyresult) {
+        return NULL;
+    }
+    len = tclistnum(result);
+    for (i = 0; i < len; i++) {
+        pyvalue = PyBytes_FromString(tclistval2(result, i));
+        if (!pyvalue) {
+            Py_DECREF(pyresult);
+            return NULL;
+        }
+        if (PySet_Add(pyresult, pyvalue)) {
+            Py_DECREF(pyvalue);
+            Py_DECREF(pyresult);
+            return NULL;
+        }
+        Py_DECREF(pyvalue);
+    }
+    return pyresult;
+}
+
+
+/* convert a TCLIST to a tuple */
+PyObject *
+tclist_to_tuple(TCLIST *result)
+{
+    int len, i;
+    PyObject *pyresult, *pyvalue;
+
+    len = tclistnum(result);
+    pyresult = PyTuple_New((Py_ssize_t)len);
+    if (!pyresult) {
+        return NULL;
+    }
+    for (i = 0; i < len; i++) {
+        pyvalue = PyBytes_FromString(tclistval2(result, i));
+        if (!pyvalue) {
+            Py_DECREF(pyresult);
+            return NULL;
+        }
+        PyTuple_SET_ITEM(pyresult, (Py_ssize_t)i, pyvalue);
+    }
+    return pyresult;
+}
+
+
+/* convert a TCMAP to a dict */
+PyObject *
+tcmap_to_dict(TCMAP *result)
+{
+    const char *key;
+    PyObject *pyresult, *pykey, *pyvalue;
+
+    pyresult = PyDict_New();
+    if (!pyresult) {
+        return NULL;
+    }
+    tcmapiterinit(result);
+    while ((key = tcmapiternext2(result)) != NULL) {
+        pykey = PyBytes_FromString(key);
+        pyvalue = PyBytes_FromString(tcmapget2(result, key));
+        if (!(pykey && pyvalue)) {
+            Py_XDECREF(pykey);
+            Py_XDECREF(pyvalue);
+            Py_DECREF(pyresult);
+            return NULL;
+        }
+        if (PyDict_SetItem(pyresult, pykey, pyvalue)) {
+            Py_DECREF(pykey);
+            Py_DECREF(pyvalue);
+            Py_DECREF(pyresult);
+            return NULL;
+        }
+        Py_DECREF(pykey);
+        Py_DECREF(pyvalue);
+    }
+    return pyresult;
+}
+
+
+/* convert a dict to a TCMAP */
+TCMAP *
+dict_to_tcmap(PyObject *pyitems)
+{
+    TCMAP *items;
+    PyObject *pykey, *pyvalue;
+    Py_ssize_t pos = 0;
+    const char *key, *value;
+
+    if (!PyDict_Check(pyitems)) {
+        set_error(PyExc_TypeError, "a dict is required");
+        return NULL;
+    }
+    items = tcmapnew();
+    if (!items) {
+        set_error(Error, "could not create TCMAP, memory issue?");
+        return NULL;
+    }
+    while (PyDict_Next(pyitems, &pos, &pykey, &pyvalue)) {
+        key = PyBytes_AsString(pykey);
+        value = PyBytes_AsString(pyvalue);
+        if (!(key && value)) {
+            tcmapdel(items);
+            return NULL;
+        }
+        tcmapput2(items, key, value);
+    }
+    return items;
+}
+
+
+/* merge dict args */
+PyObject *
+merge_put_args(const char *name, PyObject *pyvalue, PyObject *kwargs)
+{
+    if (pyvalue) {
+        if (!PyDict_Check(pyvalue)) {
+            return set_error(PyExc_TypeError, "a dict is required");
+        }
+        if (kwargs) {
+            if (PyDict_Merge(pyvalue, kwargs, 1)) {
+                return NULL;
+            }
+        }
+        return pyvalue;
+    }
+    else if (kwargs) {
+        return kwargs;
+    }
+    else {
+        return PyErr_Format(PyExc_TypeError, "%s() takes at least 2 arguments",
+                            name);
+    }
+}
+
+
+/*******************************************************************************
+* DBIter
+*******************************************************************************/
+
+/* DBIter */
+typedef struct {
+    PyObject_HEAD
+    PyObject *db;
+} DBIter;
+
+
+/* DBIter_tp_traverse */
+static int
+DBIter_tp_traverse(DBIter *self, visitproc visit, void *arg)
+{
+    Py_VISIT(self->db);
+    return 0;
+}
+
+
+/* DBIter_tp_clear */
+static int
+DBIter_tp_clear(DBIter *self)
+{
+    Py_CLEAR(self->db);
+    return 0;
+}
+
+
+/* DBIter_tp_dealloc */
+static void
+DBIter_tp_dealloc(DBIter *self)
+{
+    DBIter_tp_clear(self);
+    Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+
+/* DBIter_tp_new */
+static PyObject *
+DBIter_tp_new(PyTypeObject *type, PyObject *db)
+{
+    DBIter *self = (DBIter *)type->tp_alloc(type, 0);
+    if (!self) {
+        return NULL;
+    }
+    /* self->db */
+    Py_INCREF(db);
+    self->db = db;
+    return (PyObject *)self;
+}
+
+
+/* DBIter.__length_hint__ */
+PyDoc_STRVAR(DBIter_length_hint_doc,
+"Private method returning an estimate of len(list(hdb)).");
+
+static PyObject *
+DBIter_length_hint(DBIter *self)
+{
+    return PyLong_FromSsize_t(PyMapping_Length(self->db));
+}
+
+
+/* DBIter_tp_methods */
+static PyMethodDef DBIter_tp_methods[] = {
+    {"__length_hint__", (PyCFunction)DBIter_length_hint, METH_NOARGS,
+     DBIter_length_hint_doc},
+    {NULL}  /* Sentinel */
+};
+
+
+#endif /* _TOKYO_PYTHON_H */
---
+++ branches/tokyo-python-TDBQueryIter/test/test_MDB.py
@@ -0,0 +1,175 @@
+import unittest
+import sys
+
+from tokyo.cabinet import MDB, Error
+
+
+class MDBTest(unittest.TestCase):
+
+    def setUp(self):
+        self.db = MDB()
+
+    def tearDown(self):
+        self.db = None
+
+
+class MDBTestDict(MDBTest):
+
+    def test_contains(self):
+        self.assertRaises(TypeError, self.db.__contains__)
+        self.assertRaises(TypeError, self.db.__contains__, 1)
+        self.assertTrue(not (b"a" in self.db))
+        self.assertTrue(b"a" not in self.db)
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.assertTrue(b"a" in self.db)
+        self.assertTrue(b"b" in self.db)
+        self.assertTrue(b"c" not in self.db)
+
+    def test_len(self):
+        self.assertEqual(len(self.db), 0)
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.assertEqual(len(self.db), 2)
+
+    def test_getitem(self):
+        self.assertRaises(TypeError, self.db.__getitem__)
+        self.assertRaises(TypeError, self.db.__getitem__, 1)
+        self.assertRaises(KeyError, self.db.__getitem__, b"a")
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.assertEqual(self.db[b"a"], b"1")
+        self.assertEqual(self.db[b"b"], b"2")
+        self.db[b"c"] = b"3"
+        self.db[b"a"] = b"4"
+        self.assertEqual(self.db[b"c"], b"3")
+        self.assertEqual(self.db[b"a"], b"4")
+
+    def test_setitem(self):
+        self.assertRaises(TypeError, self.db.__setitem__)
+        self.assertRaises(TypeError, self.db.__setitem__, b"a")
+        self.assertRaises(TypeError, self.db.__setitem__, b"a", 1)
+        self.assertRaises(TypeError, self.db.__setitem__, 1, b"a")
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.assertEqual(len(self.db), 2)
+        d = dict(self.db.iteritems())
+        self.assertEqual(d, {b"a": b"1", b"b": b"2"})
+        self.db[b"c"] = b"3"
+        self.db[b"a"] = b"4"
+        d = dict(self.db.iteritems())
+        self.assertEqual(d, {b"a": b"4", b"b": b"2", b"c": b"3"})
+        del self.db[b"b"]
+        self.assertEqual(len(self.db), 2)
+        d = dict(self.db.iteritems())
+        self.assertEqual(d, {b"a": b"4", b"c": b"3"})
+
+    def test_clear(self):
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.assertEqual(len(self.db), 2)
+        self.db.clear()
+        self.assertEqual(len(self.db), 0)
+        d = dict(self.db.iteritems())
+        self.assertEqual(d, {})
+
+
+class MDBTestIter(MDBTest):
+
+    def test_iter(self):
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.db[b"c"] = b"3"
+        i = iter(self.db)
+        self.assertTrue(b"a" in i)
+        i = iter(self.db)
+        self.assertEqual([b"a", b"b", b"c"], sorted(i))
+        i = iter(self.db)
+        a = next(i)
+        b = next(i)
+        c = next(i)
+        self.assertRaises(StopIteration, next, i)
+        self.assertEqual([b"a", b"b", b"c"], sorted((a, b, c)))
+        i = iter(self.db)
+        a = next(i)
+        del self.db[b"b"]
+        self.assertRaises(Error, next, i)
+        i = iter(self.db)
+        a = next(i)
+        self.db[b"d"] = b"4"
+        self.assertRaises(Error, next, i)
+        i = iter(self.db)
+        a = next(i)
+        del self.db[b"d"]
+        self.db[b"d"] = b"5"
+        self.assertRaises(Error, next, i)
+
+    def test_iterkeys(self):
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.db[b"c"] = b"3"
+        self.assertEqual([b"a", b"b", b"c"],
+                         sorted(list(self.db.iterkeys())))
+
+    def test_itervalues(self):
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.db[b"c"] = b"3"
+        self.assertEqual([b"1", b"2", b"3"],
+                         sorted(list(self.db.itervalues())))
+
+    def test_iteritems(self):
+        self.db[b"a"] = b"1"
+        self.db[b"b"] = b"2"
+        self.db[b"c"] = b"3"
+        self.assertEqual({b"a": b"1", b"b": b"2", b"c": b"3"},
+                         dict(self.db.iteritems()))
+
+
+class MDBTestPut(MDBTest):
+
+    def test_put(self):
+        self.db.put(b"a", b"1")
+        self.db.put(b"b", b"2")
+        self.assertEqual(self.db[b"a"], b"1")
+        self.assertEqual(self.db[b"b"], b"2")
+        self.db.put(b"c", b"3")
+        self.db.put(b"a", b"4")
+        self.assertEqual(self.db[b"c"], b"3")
+        self.assertEqual(self.db[b"a"], b"4")
+
+    def test_putkeep(self):
+        self.db.putkeep(b"a", b"1")
+        self.assertRaises(KeyError, self.db.putkeep, b"a", b"1")
+
+    def test_putcat(self):
+        self.db.putcat(b"a", b"1")
+        self.db.putcat(b"a", b"2")
+        self.assertEqual(self.db[b"a"], b"12")
+
+
+class MDBTestMisc(MDBTest):
+
+    def test_searchkeys(self):
+        self.db[b"key1"] = b"1"
+        self.db[b"key2"] = b"2"
+        self.db[b"key3"] = b"3"
+        self.db[b"akey"] = b"4"
+        self.assertEqual(self.db.searchkeys(b"k"), frozenset((b"key1", b"key2", b"key3")))
+        self.assertEqual(self.db.searchkeys(b"k", 2), frozenset((b"key1", b"key2")))
+        self.assertEqual(self.db.searchkeys(b"z"), frozenset())
+        self.assertEqual(self.db.searchkeys(b"a"), frozenset((b"akey",)))
+
+
+all_tests = (
+             "MDBTestDict",
+             "MDBTestIter",
+             "MDBTestPut",
+             "MDBTestMisc",
+            )
+
+suite = unittest.TestLoader().loadTestsFromNames(all_tests,
+                                                 sys.modules[__name__])
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=2).run(suite)
