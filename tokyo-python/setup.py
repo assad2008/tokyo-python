711586c2c53b512b57979d1764e801df17ce769e
lekmalek <lekmalek@04fc1989-d439-828d-421d-ebc0a24ad0dc>
2010-07-19 11:35:41 +0000

    version checking in setup.py

--- tokyo-python/setup.py
+++ tokyo-python/setup.py
@@ -21,6 +21,7 @@
 
 from platform import python_version
 from os import name as platform_name
+from ctypes import cdll, c_char_p
 from ctypes.util import find_library
 from sys import argv
 from distutils.command.build_ext import build_ext as _build_ext
@@ -37,24 +38,49 @@ if platform_name != "posix":
     raise SystemExit("Aborted: platform '{0}' not supported".format(platform_name))
 
 
-cabinet_ext = Extension("tokyo.cabinet", ["src/cabinet.c"],
-                        libraries=["tokyocabinet", "z", "bz2", "rt", "pthread",
-                                   "m", "c"])
-tyrant_ext = Extension("tokyo.tyrant", ["src/tyrant.c"],
-                       libraries=["tokyotyrant", "tokyocabinet", "z", "bz2",
-                                  "resolv", "nsl", "dl", "rt", "pthread", "m", "c"])
+class TokyoPythonExt(object):
+    def __init__(self, required, libname, name, version, version_char_p, url,
+                 *args, **kwargs):
+        self.required = required
+        self.libname = libname
+        self.name = name
+        self.version = version
+        self.version_char_p = version_char_p
+        self.url = url
+        self.extension = Extension(*args, **kwargs)
 
+def check_extension(ext):
+    lib = find_library(ext.libname)
+    if ext.required and not lib:
+        raise SystemExit(
+            "Aborted: tokyo-python requires {0} >= {1} to be installed.\n"
+            "See {2} for more information.".format(ext.name, ext.version, ext.url))
+    elif lib:
+        if c_char_p.in_dll(cdll.LoadLibrary(lib), ext.version_char_p).value < ext.version:
+            raise SystemExit(
+                "Aborted: tokyo-python requires {0} >= {1}.".format(ext.name,
+                                                                    ext.version))
+    return lib
+
+cabinet_ext = TokyoPythonExt(True, "tokyocabinet", "Tokyo Cabinet", "1.4.45",
+                             "tcversion", "http://1978th.net/tokyocabinet/",
+                             "tokyo.cabinet", ["src/cabinet.c"],
+                             libraries=["tokyocabinet", "z", "bz2", "rt",
+                                        "pthread", "m", "c"])
+
+tyrant_ext = TokyoPythonExt(False, "tokyotyrant", "Tokyo Tyrant", "1.1.40",
+                            "ttversion", "http://1978th.net/tokyotyrant/",
+                            "tokyo.tyrant", ["src/tyrant.c"],
+                            libraries=["tokyotyrant", "tokyocabinet", "z", "bz2",
+                                       "resolv", "nsl", "dl", "rt", "pthread",
+                                       "m", "c"])
 
 class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         if "sdist" not in argv:
-            if not find_library("tokyocabinet"):
-                raise SystemExit(
-                    "Aborted: tokyo-python requires Tokyo Cabinet to be installed.\n"
-                    "See http://1978th.net/tokyocabinet/ for more information.")
-            if find_library("tokyotyrant"):
-                self.extensions.append(tyrant_ext)
+            self.extensions = [ext.extension for ext in (cabinet_ext, tyrant_ext)
+                               if check_extension(ext)]
 
 
 setup(
@@ -70,7 +96,6 @@ setup(
       license="GNU General Public License (GPL)",
       packages = ["tokyo"],
       cmdclass={"build_ext": build_ext},
-      ext_modules=[cabinet_ext],
       classifiers=[
                    "Development Status :: 4 - Beta",
                    "Intended Audience :: Developers",
